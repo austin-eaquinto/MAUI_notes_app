e9b3ef369b207bfbd21fa532b90717773378eafe
austin-eaquinto <157089105+austin-eaquinto@users.noreply.github.com>
2025-09-29 14:52:32 -0600

    Initial commit

---
+++ Notes/MauiProgram.cs
@@ -0,0 +1,30 @@
+﻿/* This is a code file that bootstraps your app. The code in this file serves 
+ * as the cross-platform entry point of the app, which configures and starts 
+ * the app. The template startup code points to the App class defined by the 
+ * App.xaml file. */
+
+using Microsoft.Extensions.Logging;
+
+namespace Notes
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
